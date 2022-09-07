acbe4c6ea8dc7149f9adffca92bd6196b67da661
mohammed sameer ahmed <sameerahmed976@gmail.com>
2022-09-07 16:11:39 +0530

    till the button added

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-const url = "https://course-api.com/react-store-products";
+import { products } from "./product.js";
+const productsContainer = document.querySelector(".products");
+const form = document.querySelector(".form");
+const formSearch = document.querySelector(".form-search");
+const btnGroup = document.querySelector(".btn-group");
+const buttons = ["all", ...new Set(products.map((product) => product.company))];
+// console.log("🚀 ~ file: script.js ~ line 7 ~ buttons", buttons);
 
-const products = async () => {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
+// console.log(products);
+// copy of the products
+let filterProducts = [...products];
+// console.log(filterProducts);
+
+const getProducts = () => {
+  if (filterProducts.length < 1) {
+    productsContainer.innerHTML = `There are no products to display. `;
+    return;
+  }
+
+  productsContainer.innerHTML = filterProducts
+    .map((product) => {
+      const { id, image, price, title } = product;
+
+      return `  <article class="card-product"  data-id=${id}  >
+          <img
+            src=${image}
+            alt="product-image"
+            class="product-image"
+          />
+          <h2 class="product-title">${title}</h2>
+          <p class="product-price">${price}</p>
+        </article>`;
+    })
+    .join(" ");
+};
+getProducts();
+
+form.addEventListener("keyup", (e) => {
+  e.preventDefault();
+
+  const input = formSearch.value;
+  filterProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(input);
+  });
+
+  getProducts();
+});
+
+const buttonsDisplay = () => {
+  btnGroup.innerHTML = buttons
+    .map((btn) => {
+      return `<button class="btn">${btn}</button>`;
+    })
+    .join("");
 };
 
-products();
+buttonsDisplay();
